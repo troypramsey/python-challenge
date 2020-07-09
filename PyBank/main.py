88b388f283dd2df4ca5b9692e5ee9ce224b1059b
Troy Ramsey <troypramsey@gmail.com>
2020-07-08 23:18:14 -0700

    fixed average change variable and change list

--- PyBank/main.py
+++ PyBank/main.py
@@ -23,38 +23,54 @@ with open(csv_path, 'r') as csv_file:
 
     # Iterating through each row in the csv
     for row in csvreader:
+        
         # Incrementing Total Months
         total_months += 1
+        
         # Incrementing End Total
-        net_total += int(row[-1])
+        net_total += int(row[1])
+        
         # Using a list to keep track of previous changes
-        amounts_lst.append(int(row[-1]))
+        amounts_lst.append(int(row[1]))
+        
         # Calculating the change between current row and previous row
         if len(amounts_lst) < 2:
-            change = int(row[-1])
+            continue
         else:
             change = amounts_lst[-1] - amounts_lst[-2]
-        change_lst.append(change)
+            change_lst.append(change)
+        
         # Incrementing Greatest Increase In Profits
         if change > greatest_increase:
             greatest_increase = change
             greatest_increase_date = row[0]
+        
         # Incrementing Greatest Decrease In Profits
         if change < greatest_decrease:
             greatest_decrease = change
             greatest_decrease_date = row[0]
+        
         # Keeping list at two items to maximize memory
         if len(amounts_lst) > 2:
             amounts_lst.pop(0)
 
     # Calculating Average Change
-    # >>> average_change = round((sum(change_lst) / len(change_lst)), 2)
+    average_change = round((sum(change_lst) / len(change_lst)), 2)
 
-    # Printing out the final analysis
-    print("Financial Analysis")
-    print('----------------------------')
-    print(f"Total Months: {total_months}")
-    print(f"Total: {net_total}")
-    print(f"Average Change: {average_change}")
-    print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
-    print(f"Greatest Increase in Profits: {greatest_decrease_date} (${greatest_decrease})")
+    # Assigning the final analysis to a variable
+    analysis = (
+        f"Financial Analysis\n"
+        f"----------------------------\n"
+        f"Total Months: {total_months}\n"
+        f"Total: ${net_total}\n"
+        f"Average Change: ${average_change}\n"
+        f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n"
+        f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})"
+    )
+
+    # Printing the analysis to the terminal
+    print(analysis)
+
+    # Writing the analysis to an external .txt
+    with open('budget_analysis.txt', 'w') as file:
+        file.write(analysis)
